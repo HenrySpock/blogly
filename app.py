e8c272b1d9e8b9453a1da93e2216e40c9ccd0096
kodai <michaelhenrytillman@mail.com>
2022-11-14 14:33:28 -0600

    delete

--- app.py
+++ app.py
@@ -18,6 +18,11 @@ connect_db(app)
 db.create_all()
 
 @app.route('/')
+def red_users():
+    """Shows list of all users in db.""" 
+    return redirect('/users')
+
+@app.route('/users')
 def list_users():
     """Shows list of all users in db."""
     users = User.query.all()
@@ -62,7 +67,7 @@ def show_user(user_id):
 #     user = User.query.get_or_404(user_id)
 #     return render_template('users/edit.html', user=user)
 
-@app.route('/users/<int:user_id>/edit_user')
+@app.route('/users/<int:user_id>/edit_user', methods=["GET"])
 def users_edit(user_id):
     """Show a form to edit an existing user"""
 
@@ -70,7 +75,7 @@ def users_edit(user_id):
     return render_template('users/edit_user.html', user=user)
 
 
-@app.route('/users/<int:user_id>/edit', methods=["POST"])
+@app.route('/users/<int:user_id>/edit_user', methods=["POST"])
 def users_update(user_id):
     """Handle form submission for updating an existing user"""
 
@@ -80,6 +85,23 @@ def users_update(user_id):
     user.image_url = request.form['image_url']
 
     db.session.add(user)
+    db.session.commit()
+
+    return redirect(f"/{user.id}")
+
+@app.route('/users/<int:user_id>/delete', methods=["GET"])
+def delete(user_id):
+    """Get route for form submission for deleting an existing user"""
+
+    user = User.query.get_or_404(user_id)
+    return redirect("/users")
+
+@app.route('/users/<int:user_id>/delete', methods=["POST"])
+def users_destroy(user_id):
+    """Handle form submission for deleting an existing user"""
+
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
     db.session.commit()
 
     return redirect("/users")
